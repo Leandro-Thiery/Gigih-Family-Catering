e12a069bfb35c129f0e3abc3c21317cdc1b03dcf
Leandro Thiery <leandrothiery23@gmai.com>
2022-04-18 00:06:38 +0700

    add test for Category, name must be unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Category, type: :model do
     category = FactoryBot.build(:category, name: nil)
     expect(category).to be_invalid
   end
+
+  it 'is invalid with a duplicate name' do
+    category1 = FactoryBot.create(:category, name: "Food")
+    category2 = FactoryBot.build(:category, name: "Food")
+    expect(category2).to be_invalid
+  end
 end
