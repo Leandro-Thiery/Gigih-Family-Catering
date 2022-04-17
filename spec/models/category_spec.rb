ccdee78caaefba461361165caab9147105378237
Leandro Thiery <leandrothiery23@gmai.com>
2022-04-18 00:10:02 +0700

    add details in test for Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,11 +10,13 @@ RSpec.describe Category, type: :model do
   it 'is invalid without a name' do
     category = FactoryBot.build(:category, name: nil)
     expect(category).to be_invalid
+    expect(category.errors[:name]).to include("can't be blank")
   end
 
   it 'is invalid with a duplicate name' do
     category1 = FactoryBot.create(:category, name: "Food")
     category2 = FactoryBot.build(:category, name: "Food")
     expect(category2).to be_invalid
+    expect(category2.errors[:name]).to include("has already been taken")
   end
 end
