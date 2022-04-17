d1372323e27653c72ccf438a3733116b1188386d
Leandro Thiery <leandrothiery23@gmai.com>
2022-04-17 23:36:17 +0700

    add validation for MenuItem name, must be unique

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -20,6 +20,13 @@ RSpec.describe MenuItem, type: :model do
       expect(menuItem.errors[:price]).to include("can't be blank")
     end
 
+    it 'is invalid with duplicate name' do 
+      menuItem1 = FactoryBot.create(:menu_item, name: "Nasi Uduk")
+      menuItem2 = FactoryBot.build(:menu_item, name: "Nasi Uduk")
+      expect(menuItem2).to be_invalid
+      expect(menuItem2.errors[:name]).to include("has already been taken")
+    end
+
     it 'is invalid with a price less than 0.01' do
       menuItem = FactoryBot.build(:menu_item, price: 0.009)
       expect(menuItem).to be_invalid
