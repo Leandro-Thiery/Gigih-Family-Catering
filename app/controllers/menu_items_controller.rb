6fd08ed7630ad02116822623cb9f00245f858a8a
Leandro Thiery <leandrothiery23@gmai.com>
2022-04-21 01:06:22 +0700

    add new, edit and create method for MenuItem

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,11 +1,29 @@
 class MenuItemsController < ApplicationController
 
-    def index
-        @menu_items = MenuItem.all
-    end
+  def index
+    @menu_items = MenuItem.all
+  end
 
-    def show
-        @menu_item = MenuItem.find(params[:id])
-    end
-    
+  def show
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def new
+    @menu_item = MenuItem.new
+  end
+
+  def edit
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def create
+    @menu_item = MenuItem.new(menu_item_params)
+    @menu_item.save
+  end
+
+  private
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :description, :price)
+  end
 end
--- spec/controllers/menu_items_controller_spec.rb
+++ spec/controllers/menu_items_controller_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe MenuItemsController, type: :controller do
     end
   end
 
-  describe 'Get #show' do
+  describe 'GET #show' do
     it 'assigns the requested menu items to @menu_item' do
       menu_item = FactoryBot.create(:menu_item)
       get :show, params: { id: menu_item }
@@ -27,4 +27,40 @@ RSpec.describe MenuItemsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #new' do
+    it 'assigns a new MenuItem to @menu_item' do
+      get :new
+      expect(assigns(:menu_item)).to be_a_new(MenuItem)
+    end
+
+    it 'renders the :new template' do
+      get :new
+      expect(:response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'assigns the requested menu item to @menu_item' do
+      menu_item = FactoryBot.create(:menu_item)
+      get :edit, params: { id: menu_item }
+      expect(assigns(:menu_item)).to eq menu_item
+    end
+
+    it 'renders the :edit template' do
+      menu_item = FactoryBot.create(:menu_item)
+      get :edit, params: { id: menu_item }
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new menu item in db' do
+        expect { post :create, params: { menu_item: attributes_for(:menu_item) } }
+          .to change(MenuItem, :count).by(1)
+      end
+    end
+  end
+
 end
